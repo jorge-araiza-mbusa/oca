bab61108b7b6db71a52390037b3e2e2d2108b757
Araiza Esparza <jaraiza@americas.corpdir.net>
2022-06-29 15:25:31 -0400

    adding test class

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+
+public class Test{
+	static int MAX_LENGHT = 5;
+	int lenght;
+	
+	public void grow(int inches){
+		if(lenght < inches){
+			int newSize = inches + lenght;
+			lenght = newSize;
+				
+		}
+		
+	}
+} 
